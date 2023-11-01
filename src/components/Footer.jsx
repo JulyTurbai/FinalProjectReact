470341c8447aa502fc7ea0dc2022e321dd9ee4a9
July Turbay <iuliatsokur96@gmail.com>
2023-11-01 21:18:35 +0200

    last commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 <img className='footer-info__img' src="img/BankLogo.png" alt="" />
                 <p className="footer-info__number">099-8676-585-89</p>
             </div>
-            <h3 className='footer-info__developed'>Developed by July Turbai</h3>
+            <h3 className='footer-info__developed'>Powered by July Turbai</h3>
         </footer>
     );
 };
